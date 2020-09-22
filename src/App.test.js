95d5f77a8a3cd68929ee20861f9a0a8f87796b47
trangtt-1054 <tran.thu.trangb@sun-asterisk.com>
2020-09-22 14:54:55 +0700

    first test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from "./App";
+
+describe("App", () => {
+  test("renders App component", () => {
+    render(<App />);
+
+    screen.debug(); //make sure you have access to React component in your test
+  });
 });
